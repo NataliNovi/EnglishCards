a0b248603e0a7583bc315537056b4c60ff7ec5c0
Natalia Novikova <106342111+NataliNovi@users.noreply.github.com>
2022-12-19 22:58:43 +0300

    import photo

--- cards/src/components/main/main.jsx
+++ cards/src/components/main/main.jsx
@@ -1,11 +1,15 @@
 import './main.css';
+import photo from '/Users/nn/Desktop/Frontend/React/EnglishCards/cards/src/components/main/photo.webp';
 
 function Main () {
     return (
         <div className="main">
-            <img src="https://img.freepik.com/free-photo/close-up-hand-writing-notebook-top-view_23-2148888824.jpg?w=2000&t=st=1671365059~exp=1671365659~hmac=f80c2aeafc0c24b32583548b6457765c4569d8da9c862e694429a04cfb24e45a" alt="mainPhoto"></img>
+      <img src={photo} alt ="mainPhoto"></img>
             <div className="main-slogan">Studying with us</div>
         </div>
     );
 }
 export default Main;
+
+
+/*  <img src="https://img.freepik.com/free-photo/close-up-hand-writing-notebook-top-view_23-2148888824.jpg?w=2000&t=st=1671365059~exp=1671365659~hmac=f80c2aeafc0c24b32583548b6457765c4569d8da9c862e694429a04cfb24e45a" alt="mainPhoto"></img> */
